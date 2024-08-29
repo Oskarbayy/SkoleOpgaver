a8f2d33e09eb471de2362aeeb36733f9d0b75fef
Oskar Bay Blichsted Hansen <osha@vestergaardcompany.com>
2024-08-29 13:12:32 +0200

    Reupload
    Bare reuploadet med alt layout bedre forhåbentligt.

---
+++ GrundlæggendeProgrammering/DigitalClock/DigitalClock/Program.cs
@@ -0,0 +1,16 @@
+﻿using DigitalClock.Models;
+using DigitalClock.Controllers;
+
+Console.CursorVisible = false;
+
+
+
+Clock clock = new Clock();
+Thread thread = new Thread(() =>  
+{ 
+    clock.Start(); 
+});
+thread.Start();
+
+ClockController controller = new ClockController(clock);
+controller.Start();
---
+++ GrundlæggendeProgrammering/OpgaveBesvarelser/OpgaveBesvarelser.Infrastructure/Assignments.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using OpgaveBesvarelser;
+
+namespace OpgaveBesvarelser.Infrastructure;
+
+public static class Assignments
+{
+    public static Dictionary<string, Action> GetAssignments()
+    {
+        string[] scriptFiles = Directory.GetFiles("Opgaver", "*.cs");
+        foreach (string scriptFile in scriptFiles)
+        {
+            string name = Path.GetFileNameWithoutExtension(scriptFile);
+            MethodInfo method = typeof(Program).GetMethod(name, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (method != null && method.ReturnType == typeof(void) && method.GetParameters().Length == 0)
+            {
+                var action = (Action)Delegate.CreateDelegate(typeof(Action), method);
+
+                options.Add(name, action);
+            }
+            else
+            {
+                throw new InvalidOperationException($"{name} is not setup correctly");
+            }
+        }
+
+
+    }
+}
